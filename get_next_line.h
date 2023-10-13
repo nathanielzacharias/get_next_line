9c8d8b8ae92428afe41c3ce0e1a1595bffb69713
Nathaniel Zacharias <nathanielzacharias@gmail.com>
2023-10-13 13:52:51 +0800

    feat: first commit, skeleton code

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nzachari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 13:45:11 by nzachari          #+#    #+#             */
+/*   Updated: 2023/10/13 13:52:08 by nzachari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 2
+#endif //BUFFER_SIZE
+
+char	*get_next_line(int fd);
+
+#endif //GET_NEXT_LINE_H
